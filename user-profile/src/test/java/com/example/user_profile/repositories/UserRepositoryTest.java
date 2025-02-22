3290dc32ddcfdb9d21d6974aee10e9b4fe49b409
Jehad Cemanes <jehadcemanesece@gmail.com>
2025-02-23 00:15:47 +0800

    Add repository test cases

---
+++ user-profile/src/test/java/com/example/user_profile/repositories/UserRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.example.user_profile.repositories;
+
+import com.example.user_profile.entities.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    private User createUser(Long userId) {
+        User user = new User();
+        if (userId != null) user.setId(userId);
+        user.setName("Firstname Lastname");
+        user.setEmail("fullname@test.com");
+        user.setGender("male");
+        user.setBirthDate(LocalDate.of(2005, 1, 1));
+        user.setAge(20);
+        user.setRole("user");
+        return user;
+    }
+
+    @Test
+    public void testFindUserByEmail() {
+        User user = createUser(null);
+        userRepository.save(user);
+        Optional<User> result = userRepository.findByEmail(user.getEmail());
+        assertTrue(result.isPresent());
+    }
+
+    @Test
+    public void testFindUsersByName() {
+        String username = "Firstname Lastname";
+        for (int i = 1; i <= 5; i++) {
+            User user = createUser(null);
+            String[] email = user.getEmail().split("@");
+            user.setEmail(email[0] + i + "@" + email[1]);
+            if (i == 1) user.setRole("admin");
+            userRepository.save(user);
+        }
+
+        Pageable pageable = PageRequest.of(0, 3);
+        Page<User> result = userRepository.findByName(username, pageable);
+        assertEquals(3, result.getContent().size());
+        assertEquals(2, result.getTotalPages());
+        assertEquals(5, result.getTotalElements());
+    }
+}
